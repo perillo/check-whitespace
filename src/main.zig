cf686ac3fc5510e7791f37111ee7b3edb62273d0
Manlio Perillo <manlio.perillo@gmail.com>
2022-12-15 12:09:32 +0100

    Add the first working draft
    Add a working implementation of the check-whitespace command.

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,82 @@
+// Copyright 2022 Manlio Perillo. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//! Check incorrect whitespace, as done by git:
+//!   - blank-at-eol
+//!   - blank-at-eof
+//!   - space-before-tab
+
 const std = @import("std");
+const ascii = std.ascii;
+const fmt = std.fmt;
+const fs = std.fs;
+const heap = std.heap;
+const io = std.io;
+const mem = std.mem;
+const os = std.os;
+const process = std.process;
+
+var stderr = io.getStdErr().writer();
+var stdout = io.getStdOut().writer();
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var arena = heap.ArenaAllocator.init(heap.page_allocator);
+    defer arena.deinit(); // NOTE(mperillo): Can be removed
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    const allocator = arena.allocator();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    var args_it = try process.argsWithAllocator(allocator);
+    _ = args_it.skip(); // it is safe to ignore
 
-    try bw.flush(); // don't forget to flush!
+    while (args_it.next()) |path| {
+        try checkWhitespace(path);
+    }
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+fn checkWhitespace(path: []const u8) !void {
+    var buf: [4096]u8 = undefined;
+
+    var file = try fs.cwd().openFile(path, .{ .mode = .read_only });
+    var br = io.bufferedReader(file.reader());
+    const r = br.reader();
+    var lineno: usize = 0;
+    while (true) {
+        if (try r.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+            lineno += 1;
+
+            if (line.len == 0) {
+                // Empty line.
+                continue;
+            }
+
+            // In order to keep the code simple, print the same line for each
+            // of the check.  Also, only the first issue is highlighted.
+
+            if (ascii.isWhitespace(line[line.len - 1])) {
+                // Blank at eol or eof.
+                // TODO(mperillo): Add support for checking blank at eof.
+                try stderr.print(
+                    "{s} {:0>5} {s}{s}\n",
+                    .{ path, lineno, line[0 .. line.len - 1], eprintf("<SP>") },
+                );
+            }
+
+            const idx = mem.indexOfScalar(u8, line, '\t') orelse 0;
+            if (idx > 0 and ascii.isWhitespace(line[idx - 1])) {
+                // Space before tab.
+                try stderr.print(
+                    "{s} {:0>5} {s}{s}{s}\n",
+                    .{ path, lineno, line[0 .. idx - 1], eprintf("<SP><TAB>"), line[idx + 1 ..] },
+                );
+            }
+        } else {
+            break;
+        }
+    }
+}
+
+/// Mark s as an error.
+fn eprintf(comptime s: []const u8) []const u8 {
+    return "\x1b[31m" ++ s ++ "\x1b[0m";
 }
